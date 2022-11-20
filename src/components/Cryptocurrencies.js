0bf0b211255423059230e3d002ba36838ea5dd8b
Leahn Jude D'souza <leahnjd@gmail.com>
2022-11-20 19:29:23 +0530

    Crypto Search bar integrated

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -7,39 +7,66 @@ import { getStats } from '../axios/Api/cryptoApi';
 
 const Cryptocurrencies = (props) => {
   const count = props.simplified ? 10 : 100;
-  const [ coins, setCoins ] = useState([]); 
+  const [ coinData, setCoinData ] = useState([]);
+  const [ coins, setCoins ] = useState([]);
+  const [ isFetching, setIsFetching ] = useState(true);
+  const [ coinSearch, setCoinSearch] = useState('');
 
   useEffect(() => {
     getStats(count)
     .then((res) => {
+      setCoinData(res?.data);
       setCoins(res?.data?.data?.coins);
+      setIsFetching(false);
     })
     .catch((error) =>{
       console.log(error);
     })
-  })
+  }, []);
 
-  return (
-    <>
-      <Row gutter={[32,32]} className='crypto-card-container'>
-        {coins.map((item) =>(
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={item.rank}>
-            <Link to={`/crypto/${item.id}`}>
-              <Card 
-                title={`${item.rank}. ${item.name} (${item.symbol})`}
-                extra={<img className='crypto-image' src={item.iconUrl} />}
-                hoverable
-              >
-                <p>Price: ${millify(item.price)}</p>
-                <p>Market Cap: {millify(item.marketCap)}</p>
-                <p>Daily Change: {millify(item.change)}%</p>
-              </Card>
-            </Link>
-          </Col>
-        ))}
-      </Row>
-    </>
-  )
+  useEffect(() => {
+    searchFilter();
+  }, [coinSearch])
+
+  const searchFilter = useCallback(() => {
+    setCoins(coinData?.data?.coins);
+    const filteredData = coinData?.data?.coins?.filter((list)=> list.name.toLowerCase().includes(coinSearch.toLowerCase()));
+    setCoins(filteredData);
+  }, [coinSearch]) 
+
+  console.log('cal',coinSearch);
+
+  if(isFetching===true) {
+    return 'Loading...';
+  }
+  else {
+    return (
+      <>
+        {props.simplified ? null : 
+          <div className='search-crypto'>
+            <Input placeholder='Search Cryptocurrency' onChange={(event)=> setCoinSearch(event.target.value)} />
+          </div>
+        }
+        <Row gutter={[32,32]} className='crypto-card-container'>
+          {coins?.map((item) =>(
+            <Col xs={24} sm={12} lg={6} className='crypto-card' key={item.rank}>
+              <Link to={`/crypto/${item.uuid}`}>
+                <Card 
+                  title={`${item.rank}. ${item.name} (${item.symbol})`}
+                  extra={<img className='crypto-image' src={item.iconUrl} />}
+                  hoverable
+                >
+                  <p>Price: ${millify(item.price)}</p>  
+                  <p>Market Cap: {millify(item.marketCap)}</p>
+                  <p>Daily Change: {millify(item.change)}%</p>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </>
+    )
+  }
 }
 
 export default Cryptocurrencies;
